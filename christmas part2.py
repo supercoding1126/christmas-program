400e86c3ed41d306a71c963aaefb01e04995c201
supercoding1126 <66332641+supercoding1126@users.noreply.github.com>
2020-12-24 14:18:01 -0500

    Update christmas part2.py

--- christmas part2.py
+++ christmas part2.py
@@ -1,20 +1,35 @@
 import turtle
+import random
 
 wn = turtle.Screen()
 wn.bgcolor('green')
 
-turtle.addshape('Users/shining/Desktop/christmas game part 2/openedbox.gif')
-turtle.addshape('Users/shining/Desktop/christmas game part 2/gift_box(not open)1.gif')
+
+
+turtle.register_shape('openedbox.gif')
+turtle.register_shape('gift_box(not open)1.gif')
+turtle.register_shape('diamond.gif')
+turtle.register_shape('volcano.gif')
+
+
+gifts = 'volcano.gif','diamond.gif'
+
 
 gift_box = turtle.Turtle()
-gift_box.shape('Users/shining/Desktop/christmas game part 2/gift_box(not open)1.gif')
+gift_box.shape('gift_box(not open)1.gif')
 
 
 present_inside = turtle.Turtle()
 
 
+def change_gift():
+   present_inside.shape(random.choice(gifts))
 
+def start():
+   gift_box.shape('openedbox.gif')
 
-
+turtle.listen()
+turtle.onkeypress(change_gift,'c')
+turtle.onkeypress(start,'s')
 
 wn.mainloop()
