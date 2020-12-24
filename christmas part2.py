dbc5d34743e3865157caf4b5f303fedd35d6355e
supercoding1126 <66332641+supercoding1126@users.noreply.github.com>
2020-12-23 21:03:24 -0500

    Add files via upload

---
+++ christmas part2.py
@@ -0,0 +1,20 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('green')
+
+turtle.addshape('Users/shining/Desktop/christmas game part 2/openedbox.gif')
+turtle.addshape('Users/shining/Desktop/christmas game part 2/gift_box(not open)1.gif')
+
+gift_box = turtle.Turtle()
+gift_box.shape('Users/shining/Desktop/christmas game part 2/gift_box(not open)1.gif')
+
+
+present_inside = turtle.Turtle()
+
+
+
+
+
+
+wn.mainloop()
